0200a8f1057fbf480a1a45c5c9f2c8d0c5b43158
Akshay Singh <jobakshay21@gmail.com>
2024-07-09 11:59:24 +0530

    jsx basics

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
 function App() {
   return (
-    "Hello World"
+    // Q1. div with class large and the id largeDiv with the text Hi.
+    // <div className="large" id="largeDiv">Hi</div>
+
+    // Q2. remove the text Hi
+    // Add a label with for prop set to inputId and add any text
+    // Add an input with the id inputId, type number and value of 3 as a number
+    <div className="large" id="largeDiv">
+      <label htmlFor="inputId">Any text</label>
+      <input id="inputId" type="number" value={3} />
+    </div>
   )
 }
 
