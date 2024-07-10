132e2b71817313902e43e3d8bee4185eecc445e3
Akshay Singh <jobakshay21@gmail.com>
2024-07-10 10:37:16 +0530

    15-16 user card project

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
+import './user.css'
+import user from "./user.json"
+import { UserCard } from "./UserCard"
+import { UserCardClass } from './UserCardClass'
+
 function App() {
   return (
-    "Hello World"
-  )
+    <div>
+      <h1>User Cards</h1>
+      <h2>via function component</h2>
+      <UserCard
+        name={user.name}
+        age={user.age}
+        phoneNumber={user.phoneNumber}
+        address={user.address}
+      />
+      &nbsp;
+      <h2>via class component</h2>
+      <UserCardClass
+        name={user.name}
+        age={user.age}
+        phoneNumber={user.phoneNumber}
+        address={user.address}
+      />
+    </div>
+  );
 }
 
 export default App
