b2b0483269a16b19ffea857db2bdec4d30814a48
Akshay Singh <jobakshay21@gmail.com>
2024-07-30 12:28:44 +0530

    user list project and bonus part

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,42 @@
+import { useEffect, useState } from "react"
+import { User } from "./User"
+
 function App() {
+  const [users, setUsers] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    setLoading(true)
+    const controller = new AbortController()
+    fetch("https://jsonplaceholder.typicode.com/users", {
+      signal: controller.signal,
+    })
+      .then((data) => {
+        return data.json()
+      })
+      .then(setUsers)
+      .finally(() => {
+        setLoading(false)
+      })
+    
+    return () => {
+      controller.abort()
+    }
+  }, [])
+
   return (
-    "Hello World"
+    <>
+      <h1>User list project</h1>
+      {loading ? (
+        <p>loading currently...</p>
+      ) : (
+        <ul>
+          {users.map((user) => {
+            return <User key={user.id} name={user.name} />
+          })}
+        </ul>
+      )}
+    </>
   )
 }
 
---
+++ src/User.jsx
@@ -0,0 +1,6 @@
+import React from "react"
+export function User({ name }) {
+    return (
+        <li>{name}</li>
+    )
+}
