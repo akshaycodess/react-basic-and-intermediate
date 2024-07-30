5dde156730f6f7291268b535ee4626b744fc0076
Akshay Singh <jobakshay21@gmail.com>
2024-07-30 18:41:55 +0530

    todo list project and bonus

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,68 @@
+import { useState } from "react"
+import "./style.css"
+import { TodoItem } from "./TodoItem"
+
 function App() {
+  const [newTodoName, setNewTodoName] = useState("")
+  const [todos, setTodos] = useState([])
+
+  // add new todo item
+  function addNewTodo() {
+    if (newTodoName === "") return
+
+    setTodos((currentTodos) => {
+      return [
+        ...currentTodos,
+        { name: newTodoName, completed: false, id: crypto.randomUUID() },
+      ]
+    })
+    setNewTodoName("")
+  }
+  // to set item completed (true/fasle)
+  function toggleTodo(todoId, completed) {
+    setTodos((currentTodos) => {
+      return currentTodos.map((todo) => {
+        if (todo.id === todoId) return { ...todo, completed }
+        return todo
+      })
+    })
+  }
+  // to delete an item
+  function deleteTodo(todoId) {
+    setTodos((currentTodos) => {
+      return currentTodos.filter((todo) => todo.id !== todoId)
+    })
+  }
+
   return (
-    "Hello World"
+    <>
+      <h1>Todo List Project</h1>
+      <ul id="list">
+        {todos.map(todo => {
+          return (
+            <TodoItem
+              key={todo.id}
+              {...todo}
+              toggleTodo={toggleTodo}
+              deleteTodo={deleteTodo}
+            />
+          )
+        })}
+      </ul>
+
+      <div id="new-todo-form">
+        <label htmlFor="todo-input">
+          New Todo &nbsp;
+          <input
+            type="text"
+            id="todo-input"
+            value={newTodoName}
+            onChange={(e) => setNewTodoName(e.target.value)}
+          />
+        </label>
+        <button onClick={addNewTodo}>Add Todo</button>
+      </div>
+    </>
   )
 }
 
