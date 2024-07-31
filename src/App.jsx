70fb8a30dc05d0b12a751526bdb7f326d86e4d04
Akshay Singh <jobakshay21@gmail.com>
2024-07-31 19:05:19 +0530

    uselocalstorage project and bonus

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,60 @@
+import { useLocalStorage } from "./useLocalStorage"
+
 function App() {
+  const [firstName, setFirstName] = useLocalStorage("FIRST_NAME", "")
+
+  // Bonus:
+  const [lastName, setLastName] = useLocalStorage("LAST_NAME", () => {
+    return "Default"
+  })
+  const [hobbies, setHobbies] = useLocalStorage("HOBBIES", [
+    "Programming",
+    "Weight Lifting",
+  ])
+
   return (
-    "Hello World"
-  )
+    <>
+      <h1>useLocalStorage Project</h1>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "flex-start",
+          marginBottom: "1rem",
+        }}
+      >
+        <label>First Name</label>
+        <input
+          type="text"
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+        />
+      </div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "flex-start",
+          marginBottom: "1rem",
+        }}
+      >
+        <label>Last Name</label>
+        <input
+          type="text"
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
+        />
+      </div>
+      <div>{hobbies.join(", ")}</div>
+      <button
+        onClick={() =>
+          setHobbies((currentHobbies) => [...currentHobbies, "New hobby"])
+        }
+      >
+        Add Hobby
+      </button>
+    </>
+  );
 }
 
 export default App
